3a8e68f0f793fc2356e0bac7e4302a6e330e7a26
JeevithaParameshwaran <30828380+JeevithaParameshwaran@users.noreply.github.com>
2017-09-13 21:18:31 +0530

    Create pribt.c

---
+++ pribt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a1,a2,count=0,i,j;
+printf("Enter the intervals greater than 1"};
+scanf("%d %d",&a1,&a2);
+for(i=a1+1;i<a2;i++)
+{
+ for(j=a1+1;j<a2;j++)
+  {
+    if(i%j==0)
+    {
+     count++;
+     }
+  }
+     if(count==1)
+     { printf("%d\t",i);
+     }
+}
+}
