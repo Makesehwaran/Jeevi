a7b6a016d5765e3268e0d14efffb92669af443e5
JeevithaParameshwaran <30828380+JeevithaParameshwaran@users.noreply.github.com>
2017-09-15 19:41:56 +0530

    Create primec.c

---
+++ primec.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a,b,i,j,count=0,count1=0;
+for(i=a+1;i<b;i++)
+{
+for(j=a+1;j<b;j++_
+ {
+  if(i%j==0)
+   {
+     count++;
+     }
+     }
+     if(count==1)
+     {
+     count1++;
+     }
+    }
+    printf("The number of prime number is %d",count1++);
+    }
