bdcc007b68186504537159868d585042529c44cf
JeevithaParameshwaran <30828380+JeevithaParameshwaran@users.noreply.github.com>
2017-09-17 09:22:56 +0530

    Create digit.c

---
+++ digit.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char a[10];
+printf("Enter a string");
+scanf("%s",a);
+if(a[0]=='0'||a[0]=='1'||a[0]=='2'||a[0]=='3'||a[0]=='4'||a[0]=='5'||a[0]=='6'||a[0]=='7'||a[0]=='8'||a[0]=='9')
+printf("True");
+else
+printf("False");
+}
