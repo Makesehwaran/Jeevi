bff2910bafb73db814bf3080f28eaff42ff094ee
JeevithaParameshwaran <30828380+JeevithaParameshwaran@users.noreply.github.com>
2017-09-17 11:28:30 +0530

    Create holi.c

---
+++ holi.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char a[10];
+printf("Enter the string");
+scanf("%s",a);
+if(a=="Monday"||a=="Tuesday"||a=="Wednesday"||a=="Thursday"||a=="Friday")
+printf("true");
+else
+printf("false");
+}
