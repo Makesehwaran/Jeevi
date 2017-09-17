65d1afcbb98f02ffad5dcc678cc3524e5663cfb5
JeevithaParameshwaran <30828380+JeevithaParameshwaran@users.noreply.github.com>
2017-09-17 11:52:13 +0530

    Create dup.c

---
+++ dup.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+long int a[10];
+printf("Enter the numbers");
+for(i=0;i<10;i++)
+scanf("%ld",&a[i]);
+for(i=0;i<9;i++)
+{
+{
+if(a[i]==a[i+1])
+count++;
+}
+if(count!=0)
+printf("%ld is repeated",a[i]);
+}
+}
