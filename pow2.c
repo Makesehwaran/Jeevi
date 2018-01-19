cb93f5599b4c446d51a78b72959d3b5e79ed750b
JeevithaParameshwaran <30828380+JeevithaParameshwaran@users.noreply.github.com>
2018-01-19 08:28:10 +0000

    Create pow2.c

---
+++ pow2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main() {
+    int a,i,j=1;
+    scanf("%d",&a);
+    for(i=1;i<a;i++)
+    {
+        j=j*2;
+        if(j==a)
+         printf("yes");
+    }
+     }
+   
+
