67982ef6cd4a54184c2dc5e27f295ebb5d2852f0
JeevithaParameshwaran <30828380+JeevithaParameshwaran@users.noreply.github.com>
2018-01-19 08:20:17 +0000

    Update mul.c

--- mul.c
+++ mul.c
@@ -1,13 +1,8 @@
 #include<stdio.h>
 void main()
 {
-int j,i,n;
-printf("Enter the table number");
+int i;
+printf("Enter the number");
 scanf("%d",&i);
-printf("Enter the limit");
-scanf("%d",&n);
-for(j=1;j<=n;j++)
-{
-printf("%d*%d=%d\n",i,j,i*j);
-}
-}
+printf(%d".i++);
+ }
