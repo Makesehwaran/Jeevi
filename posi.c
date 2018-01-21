7bf70dbad8e442023083edb716340b6ce0c06f9d
JeevithaParameshwaran <30828380+JeevithaParameshwaran@users.noreply.github.com>
2018-01-21 10:18:15 +0530

    Create posi.c

---
+++ posi.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a[10],n,k,i;
+printf("Enter the number of integers:")
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("Enter the position:")
+scanf("%d",&k);
+printf("%d",a[k-1]);
+}
