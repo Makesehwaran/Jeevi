cb2cad833023466f124db639b54bcec5b998bb56
JeevithaParameshwaran <30828380+JeevithaParameshwaran@users.noreply.github.com>
2018-02-01 08:58:25 +0000

    Create join.c

---
+++ join.c
@@ -0,0 +1,12 @@
+ #include<stdio.h>
+   #include<string.h>
+void main()
+{
+    char a[4],b[4];
+    printf("Enter two number");
+    scanf("%s %s",a,b);
+    strcat(a,b);
+    printf("%s",a);
+}
+    
+
