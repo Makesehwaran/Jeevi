be6930d75c412bbab992cd5b88efbdd9a39fad00
JeevithaParameshwaran <30828380+JeevithaParameshwaran@users.noreply.github.com>
2018-02-06 16:26:19 +0530

    Create lexi.c

---
+++ lexi.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a[20];
+    int i,j,temp,b;
+    scanf("%s",a);
+    b=strlen(a);
+    for(i=0;i<b;i++)
+    {
+        for(j=i;j<b;j++)
+        {   if(a[i]>a[j])
+            {
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<b;i++)
+    {
+        printf("%c",a[i]);
+    }
+}
+
+
+
