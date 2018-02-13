e404e3d7e5503961931a574357b656dcd1d234e2
JeevithaParameshwaran <30828380+JeevithaParameshwaran@users.noreply.github.com>
2018-02-13 10:23:23 +0000

    Create odddigit.c

---
+++ odddigit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a[10],i;
+    int b;
+    printf("Enter the number");
+    scanf("%s",a);
+    b=strlen(a);
+    for(i=0;i<b;i++)
+    {
+        if(a[i]%2!=0)
+        {
+            printf("%c ",a[i]);
+        }
+    }
+}
